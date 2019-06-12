c26fd19beacd7c8b5501a95c628d68056541683f
AMarchmarti <marchmarti.antoni@gmail.com>
2019-06-12 19:03:26 +0200

    Se creara un caso test para probar las instancias

--- src/test/java/org/lasencinas/AppTest.java
+++ src/test/java/org/lasencinas/AppTest.java
@@ -1,5 +1,6 @@
 package org.lasencinas;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +14,15 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test_instance(){
+       Eyeholes eyeholes = new Eyeholes();
+
+       assertTrue(eyeholes instanceof Cereales);
+
+       assertTrue(eyeholes instanceof Item);
+
+       Cereales cereales = new Eyeholes();
+
+       assertEquals(cereales, eyeholes);
     }
 }
