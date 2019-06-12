ca40f065e0d21d5b45f3a243391183f9a766d58d
AMarchmarti <marchmarti.antoni@gmail.com>
2019-06-12 17:22:55 +0200

    Se comentara la clase para poder efectuar casos test

--- src/main/java/org/lasencinas/App.java
+++ src/main/java/org/lasencinas/App.java
@@ -12,28 +12,28 @@ public class App {
          * Desayuno Eyeholes
          */
 
-        Desayuno eyeDes = FastBreakTruck.prepararEyeHoles();
-        System.out.print("\nNo abras la caja de Eyeholes!");
-        eyeDes.mostrarItems();
-        System.out.print("Precio pedido: " + eyeDes.getCoste());
-
-        /**
-         * Desayuno Smiggles
-         */
-
-        Desayuno smigDes = FastBreakTruck.prepararSmiggles();
-        System.out.print("\nSmiggles per als nins!");
-        smigDes.mostrarItems();
-        System.out.print("Precio pedido: " + smigDes.getCoste());
-
-        /**
-         * Plumbus
-         */
-
-        eyeDes = FastBreakTruck.prepararEyeHoles();
-        FastBreakTruck.incluirJuguete(eyeDes);
-        System.out.print("\nEyeholes con plumbus!");
-        eyeDes.mostrarItems();
-        System.out.print("Precio pedido: " + eyeDes.getCoste());
-    }
+//        Desayuno eyeDes = FastBreakTruck.prepararEyeHoles();
+//        System.out.print("\nNo abras la caja de Eyeholes!");
+//        eyeDes.mostrarItems();
+//        System.out.print("Precio pedido: " + eyeDes.getCoste());
+//
+//        /**
+//         * Desayuno Smiggles
+//         */
+//
+//        Desayuno smigDes = FastBreakTruck.prepararSmiggles();
+//        System.out.print("\nSmiggles per als nins!");
+//        smigDes.mostrarItems();
+//        System.out.print("Precio pedido: " + smigDes.getCoste());
+//
+//        /**
+//         * Plumbus
+//         */
+//
+//        eyeDes = FastBreakTruck.prepararEyeHoles();
+//        FastBreakTruck.incluirJuguete(eyeDes);
+//        System.out.print("\nEyeholes con plumbus!");
+//        eyeDes.mostrarItems();
+//        System.out.print("Precio pedido: " + eyeDes.getCoste());
+//    }
 }
